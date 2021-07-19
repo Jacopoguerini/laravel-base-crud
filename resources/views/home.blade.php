cb4a63defef8348cff752c3f4a8d76866eaec1a7
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-07-19 16:36:11 +0200

    Creazione HomeController e Layouts

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('main-content')
+
+    <div class="container">
+        <h1 class="text-center">Home Page DC Comics</h1>
+        <p class="text-center">DCComics.com: Welcome to the Official Site for DC. DC is home to the "World's Greatest Super Heroes,” including SUPERMAN, BATMAN, WONDER WOMAN</p>
+    </div>
+    
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="container">
+    <p class="text-center">Footer DC Comics</p>
+</footer>
