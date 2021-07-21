4e0f6065db2618c2f1ac314e48ca424b48ae84ce
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-07-21 17:21:11 +0200

    Fix radio button

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -43,11 +43,11 @@
             <div class="mt-2">
                 <label class="me-2" for="type">Tipo di fumetto</label>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="type" id="type" value="comic book" required>
+                    <input class="form-check-input" name="type" id="type" value="comic book" type="radio" {{ ($comic->type=="comic book")? "checked" : "" }} required>
                     <label class="form-check-label" for="type">Comic Book</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="type" id="type" value="graphic novel" required>
+                    <input class="form-check-input" name="type" id="type" value="graphic novel" type="radio" {{ ($comic->type=="graphic novel")? "checked" : "" }} required>
                     <label class="form-check-label" for="type">Graphic Novel</label>
                 </div>
             </div>
